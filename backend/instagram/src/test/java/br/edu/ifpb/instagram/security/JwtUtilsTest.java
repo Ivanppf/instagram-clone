82ec4659bdb71e1b4216dd5fbb9c0fa00017beaa
Ivan Pinheiro <Ivanppf1@gmail.com>
2025-03-07 10:43:03 -0300

    fix: added beforeEach annotation in JwtUtilsTest

--- backend/instagram/src/test/java/br/edu/ifpb/instagram/security/JwtUtilsTest.java
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/security/JwtUtilsTest.java
@@ -6,9 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.session.RedisSessionProperties.ConfigureAction;
+import org.springframework.boot.logging.LoggerConfiguration.ConfigurationScope;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
@@ -18,17 +24,23 @@ public class JwtUtilsTest {
 
     @Autowired
     JwtUtils jwtUtils;
+
     @Autowired
     AuthenticationManager authenticationManager;
+
     @Mock
     Authentication authentication;
 
+    String token;
+
+    @BeforeEach
+    void setup() {
+        when(authentication.getName()).thenReturn("usuarioTeste");
+        token = jwtUtils.generateToken(authentication);
+    }
+
     @Test
     void shouldGenerateTokenSuccessfully() {
-
-        when(authentication.getName()).thenReturn("usuarioTeste");
-
-        String token = jwtUtils.generateToken(authentication);
 
         assertNotNull(token);
         assertFalse(token.isEmpty());
@@ -39,9 +51,6 @@ public class JwtUtilsTest {
 
     @Test
     void shouldReturnUserNameSuccessfully() {
-        when(authentication.getName()).thenReturn("usuarioTeste");
-
-        String token = jwtUtils.generateToken(authentication);
 
         String name = jwtUtils.getUsernameFromToken(token);
 
@@ -52,13 +61,17 @@ public class JwtUtilsTest {
     }
 
     @Test
-    void shouldValidateTokenSuccessfully() {
-
-        when(authentication.getName()).thenReturn("usuarioTeste");
-
-        String token = jwtUtils.generateToken(authentication);
+    void tokenShouldBeValid() {
 
         assertTrue(jwtUtils.validateToken(token));
+
+    }
+
+    @Test
+    void tokenShouldNotBeValid() {
+
+        String newToken = token + "a";
+        assertFalse(jwtUtils.validateToken(newToken));
 
     }
 
