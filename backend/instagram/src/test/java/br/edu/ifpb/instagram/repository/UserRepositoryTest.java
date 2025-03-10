6cf8ca9bfbe21a37985d23e1d458e83f35422262
Atos Alves <atosalvesqueiroz@gmail.com>
2025-03-10 07:59:17 -0300

    test: add UserRepositoryTest for updatePartialUser method

---
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/repository/UserRepositoryTest.java
@@ -0,0 +1,38 @@
+package br.edu.ifpb.instagram.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import br.edu.ifpb.instagram.model.entity.UserEntity;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+public class UserRepositoryTest {
+
+        @Autowired
+        UserRepository userRepository;
+
+        @Autowired
+        TestEntityManager testEntityManager;
+
+        @Test
+        void testUpdatePartialUser() {
+                var user = new UserEntity();
+                user.setEmail("email_test");
+                user.setFullName("fullname_test");
+                user.setUsername("username_test");
+                user.setEncryptedPassword("encrypted_password_test");
+
+                var savedUser = testEntityManager.persist(user);
+
+                int updatedUser = userRepository.updatePartialUser(null, "updated_email_test", null, null,
+                                savedUser.getId());
+
+                assertEquals(1, updatedUser);
+        }
+}
